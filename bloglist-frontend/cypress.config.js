04d8e498427ae208739a9e351652a0f1d4be5166
Rojhat M <rmutlu01@hotmail.com>
2023-08-07 01:20:26 +0300

    H

--- bloglist-frontend/cypress.config.js
+++ bloglist-frontend/cypress.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:3001'
   },
   env: {
-    BACKEND: 'http://localhost:3003/api'
+    BACKEND: 'http://localhost:3001/api'
   }
 })
