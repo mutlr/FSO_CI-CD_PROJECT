9bb77429324fbfc46c3b710428dbc0e7569fc797
Rojhat M <rmutlu01@hotmail.com>
2023-08-08 00:48:12 +0300

    FdD

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
