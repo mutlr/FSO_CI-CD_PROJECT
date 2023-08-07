4aba651335f9729ce7407c66f7a576d861d41a63
Rojhat M <rmutlu01@hotmail.com>
2023-08-07 23:06:04 +0300

    FdD

--- build_steps.sh
+++ build_steps.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 npm install
-cd bloglist-frontend && npm install && npm run build
-cd .. && cp -r bloglist-frontend/build/* .
+cd bloglist-frontend && npm install && npm run build && cd .. && cp -r bloglist-frontend/build/* .
