1af04ee995daf6a7f31874593532dd994938a1cc
Rojhat M <rmutlu01@hotmail.com>
2023-08-11 11:29:00 +0300

    works

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('express-async-errors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
-console.log('ENV: ', process.env.NODE_ENV )
+console.log('ENV:- ', process.env.NODE_ENV )
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
